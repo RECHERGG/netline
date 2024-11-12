7391b30c8bbd92f6935599786bbdbf0c3dee09f6
HttpMarco <mirco.lindenau@gmx.de>
2024-11-12 17:59:55 +0100

    Start codec implementation

--- src/main/java/dev/httpmarco/netline/codec/PacketDecoder.java
+++ src/main/java/dev/httpmarco/netline/codec/PacketDecoder.java
@@ -1,4 +1,13 @@
 package dev.httpmarco.netline.codec;
 
-public final class PacketDecoder {
+import io.netty5.buffer.Buffer;
+import io.netty5.channel.ChannelHandlerContext;
+import io.netty5.handler.codec.ByteToMessageDecoder;
+
+public final class PacketDecoder extends ByteToMessageDecoder {
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, Buffer buffer) throws Exception {
+
+    }
 }
--- src/main/java/dev/httpmarco/netline/codec/PacketEncoder.java
+++ src/main/java/dev/httpmarco/netline/codec/PacketEncoder.java
@@ -1,4 +1,19 @@
 package dev.httpmarco.netline.codec;
 
-public final class PacketEncoder {
+import dev.httpmarco.netline.packet.Packet;
+import io.netty5.buffer.Buffer;
+import io.netty5.channel.ChannelHandlerContext;
+import io.netty5.handler.codec.MessageToByteEncoder;
+
+public final class PacketEncoder extends MessageToByteEncoder<Packet> {
+
+    @Override
+    protected Buffer allocateBuffer(ChannelHandlerContext channelHandlerContext, Packet packet) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Packet packet, Buffer buffer) throws Exception {
+
+    }
 }

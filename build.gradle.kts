c0542a7151770325ee857d18ba0d4700c23909c9
HttpMarco <mirco.lindenau@gmx.de>
2024-11-20 21:09:12 +0100

    Implement deployment lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    alias(libs.plugins.nexus.publish)
+
 }
 
 group = "dev.httpmarco"
@@ -23,4 +25,51 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+allprojects {
+    apply(plugin = "maven-publish")
+}
+
+extensions.configure<PublishingExtension> {
+    publications {
+        create("library", MavenPublication::class.java) {
+            from(project.components.getByName("java"))
+
+            pom {
+                name.set(project.name)
+                url.set("https://github.com/httpmarco/netline")
+                description.set("Simple network library for Java, based on Netty 5")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mirco Lindenau")
+                        email.set("mirco.lindenau@gmx.de")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/httpmarco/netline")
+                    connection.set("https://github.com/httpmarco/netline.git")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            username.set(System.getenv("ossrhUsername")?.toString() ?: "")
+            password.set(System.getenv("ossrhPassword")?.toString() ?: "")
+        }
+    }
+    useStaging.set(!project.rootProject.version.toString().endsWith("-SNAPSHOT"))
 }
